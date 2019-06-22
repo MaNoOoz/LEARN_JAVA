e646f8e6ecc64650733cb699846bd77cddecce1f
MaNoOox <manoo.sar@gmail.com>
2019-06-22 21:42:45 +0300

    Ep 0: Q2 : https://www.codewars.com/kata/exes-and-ohs/solutions/java
    ================== Best Practices =================

--- src/learn/java/oop/Main.java
+++ src/learn/java/oop/Main.java
@@ -9,34 +9,9 @@ public class Main {
     }
 
     public static Boolean findAnswer(String str) {
-        int numOfO = 0;
-        int numOfX = 0;
-        String s = str.toLowerCase();
-        char[] ch = s.toCharArray();
-        for (int i = 0; i < ch.length; i++) {
-            switch (ch[i]) {
-                case 'o':
-                    numOfO++;
-                    break;
-                case 'x':
-                    numOfX++;
-                    break;
-            }
-        }
-        System.out.println("The Number Of O's Is " + numOfO + "");
-        System.out.println("The Number Of X's Is " + numOfX + "");
-        int total = numOfO + numOfX;
+        str = str.toLowerCase();
+        return str.replace("o", "").length() == str.replace("x", "").length();
 
-        if (numOfO == numOfX && total > 2) {
-            System.out.println(s);
-            return true;
-
-        } else if (total == 0) {
-            System.out.println(s);
-            return true;
-        } else
-            System.out.println(s);
-        return false;
 
 
     }
